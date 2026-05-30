b99cbf18da630987ec7dd88880a9e6031170f938
Ricardo Junior <112292689+ricxJr@users.noreply.github.com>
2026-05-30 11:02:06 -0300

    feat: conector de namespace (#130)
    * feat: conector de namespace

--- src/ccs/index.ts
+++ src/ccs/index.ts
@@ -33,3 +33,5 @@ export { createItem } from "./commands/createItem";
 export { convertCurrentItem, convertCurrentItemCustom, convertCurrentItemOnSave } from "./commands/converterItem";
 export { analizarVersaoItem } from "./commands/analizarVersaoItem";
 export { atualizarConfiguracoes } from "./commands/atualizarConfiguracoes";
+export { activateSiblingFolders, reactivateNamespaceConnections } from "./connectionActivator";
+export type { ReactivationResult } from "./connectionActivator";
